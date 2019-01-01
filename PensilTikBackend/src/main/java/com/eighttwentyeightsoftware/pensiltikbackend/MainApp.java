931d1919860406772e5b08dc2a5df34ed2f04bf4
Plamedi L. Lusembo <plam.l@live.fr>
2019-01-01 20:27:01 -0200

    Primeiro commit de fato

--- PensilTikBackend/src/main/java/com/eighttwentyeightsoftware/pensiltikbackend/MainApp.java
+++ PensilTikBackend/src/main/java/com/eighttwentyeightsoftware/pensiltikbackend/MainApp.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApp {
     public static void main(String[] args) {
-
-        System.out.println("");
         SpringApplication.run(MainApp.class, args);
     }
 }
