edc9da3bde914ab9bf4385b4dbbe54894e55665b
Plamedi L. Lusembo <plam.l@live.fr>
2019-01-07 21:14:23 -0200

    Primeiro codigo de javafx

--- PensilTikFrontend/src/main/java/com/eighttwentyeightsoftware/pensiltikfrontend/MainApp.java
+++ PensilTikFrontend/src/main/java/com/eighttwentyeightsoftware/pensiltikfrontend/MainApp.java
@@ -1,8 +1,32 @@
 package com.eighttwentyeightsoftware.pensiltikfrontend;
 
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class MainApp extends Application {
+
     public static void main(String[] args) {
-        System.out.println("Teste");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(new URL("C:/Users/Plamedi L. Lusembo/ETES Desenv/8.28-Software/PensilTikFrontend/src/main/java/com/eighttwentyeightsoftware/pensiltikfrontend/view/hello-world.fxml"));
+        VBox vbox = loader.<VBox>load();
+
+        Scene scene = new Scene(vbox);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
